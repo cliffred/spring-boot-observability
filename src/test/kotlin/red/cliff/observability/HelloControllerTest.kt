e8a448e652db7704a3ee8a996ec3d746ae696f2f
Cliffred van Velzen <cliffred@cliffred.nl>
2024-01-18 13:07:47 +0100

    Add test report on failure with a failing test for testing

--- src/test/kotlin/red/cliff/observability/HelloControllerTest.kt
+++ src/test/kotlin/red/cliff/observability/HelloControllerTest.kt
@@ -2,6 +2,7 @@ package red.cliff.observability
 
 import com.fasterxml.jackson.databind.JsonNode
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -21,5 +22,9 @@ class HelloControllerTest(
             .expectStatus().isOk
             .expectBody<JsonNode>()
             .consumeWith { println(it.responseBody?.toPrettyString()) }
+    }
+
+    should("fail") {
+        true shouldBe false
     }
 })
