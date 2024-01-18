9f075372619d3ac8d2466bc951048e651311a694
Cliffred van Velzen <cliffred@cliffred.nl>
2024-01-18 23:34:54 +0100

    Use version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
-    id("org.springframework.boot") version "3.2.1"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("org.jmailen.kotlinter") version "4.2.0"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlinter)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.spring)
 }
 
 group = "red.cliff"
@@ -21,38 +21,25 @@ repositories {
 }
 
 dependencies {
-    val logback = "0.1.5"
-    val lokiAppender = "1.4.2"
-    val kotest = "5.8.0"
-    val kotestSpring = "1.1.3"
-    val springMockk = "4.0.2"
-    val kotlinLogging = "6.0.3"
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.bundles.kotlin)
 
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.micrometer:micrometer-tracing-bridge-otel")
-    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
-    implementation("io.github.oshai:kotlin-logging-jvm:$kotlinLogging")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation(libs.kotlin.logging)
+    implementation(libs.bundles.logging)
+    implementation(libs.bundles.tracing)
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    developmentOnly(libs.spring.boot.devtools)
+    developmentOnly(libs.spring.boot.docker.compose)
 
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-    runtimeOnly("ch.qos.logback.contrib:logback-jackson:$logback")
-    runtimeOnly("ch.qos.logback.contrib:logback-json-classic:$logback")
-    runtimeOnly("com.github.loki4j:loki-logback-appender:$lokiAppender")
+    runtimeOnly(libs.micrometer.registry.prometheus)
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.kotest:kotest-runner-junit5:$kotest")
-    testImplementation("io.kotest:kotest-assertions-core:$kotest")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestSpring")
-    testImplementation("com.ninja-squad:springmockk:$springMockk")
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.bundles.kotest)
+    testImplementation(libs.spring.mockk)
 
-    testRuntimeOnly("org.springframework.boot:spring-boot-starter-webflux")
+    testRuntimeOnly(libs.spring.boot.starter.webflux)
 }
 
 tasks.withType<KotlinCompile> {
