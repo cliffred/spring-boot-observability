bd1c8787a1e625847b23fd7dd6893d8d3429efad
Cliffred van Velzen <cliffred@cliffred.nl>
2024-01-21 21:23:20 +0100

    Add k6 load test

--- k6/script.js
+++ k6/script.js
@@ -1,12 +1,25 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import {check, sleep} from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s'
+    thresholds: {
+        http_req_failed: [{threshold: 'rate<0.01', abortOnFail: true}],
+        http_req_duration: ['p(99)<500'], // 99% of requests should be below 500ms
+    },
+    scenarios: {
+        load_test: {
+            executor: 'ramping-vus',
+            stages: [
+                {duration: '10s', target: 20},
+                {duration: '20s', target: 20},
+                {duration: '5s', target: 0},
+            ],
+        },
+    },
 };
 
-export default function() {
-  http.get('http://localhost:8080/customers');
-  sleep(1);
+export default function () {
+    const res = http.get('http://localhost:8080/customers');
+    check(res, {'status was 200': (r) => r.status == 200});
+    sleep(1);
 }
