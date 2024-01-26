bc3682c2ad9fc26553c83a26ae7b437fd83cf8f2
Cliffred van Velzen <cliffred@cliffred.nl>
2024-01-26 18:19:17 +0100

    Inject current username into the current span

--- src/main/kotlin/config/TracerInjectionFilter.kt
+++ src/main/kotlin/config/TracerInjectionFilter.kt
@@ -5,19 +5,25 @@ import jakarta.servlet.Filter
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
 import org.springframework.stereotype.Component
 import kotlin.random.Random
 
 private const val CUSTOM_TX_ID = "custom_tx_id"
 
 @Component
-class InjectCustomTxIdInBaggageFilter(private val tracer: Tracer) : Filter {
+class TracerInjectionFilter(private val tracer: Tracer) : Filter {
     override fun doFilter(
         request: ServletRequest,
         response: ServletResponse,
-        chain: FilterChain
+        chain: FilterChain,
     ) {
         tracer.createBaggageInScope(CUSTOM_TX_ID, Random.nextInt(100, 1000).toString())
+
+        val currentUser = (SecurityContextHolder.getContext().authentication.principal as? User)?.username ?: "-"
+        tracer.currentSpan()?.tag("username", currentUser)
+
         return chain.doFilter(request, response)
     }
 }
