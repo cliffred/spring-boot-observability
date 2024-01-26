b44bdf195970d5bf7e93b470337897bbfb1d0944
Cliffred van Velzen <cliffred@cliffred.nl>
2024-01-26 15:49:42 +0100

    Add Spring security with basic authentication using JDBC and the H2 db

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ repositories {
 dependencies {
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.data.jdbc)
+    implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.jackson.module.kotlin)
     implementation(libs.bundles.kotlin)
---
+++ src/main/kotlin/config/SecurityConfig.kt
@@ -0,0 +1,52 @@
+package red.cliff.observability.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl
+import org.springframework.security.provisioning.JdbcUserDetailsManager
+import org.springframework.security.provisioning.UserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import javax.sql.DataSource
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeRequests {
+                authorize("/api/**", authenticated)
+                authorize(anyRequest, permitAll)
+            }
+            httpBasic { }
+        }
+        return http.build()
+    }
+
+    @Bean
+    fun dataSource(): DataSource {
+        return EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+            .build()
+    }
+
+    @Bean
+    fun users(dataSource: DataSource): UserDetailsManager {
+        val user =
+            User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build()
+        val userManager = JdbcUserDetailsManager(dataSource)
+        userManager.createUser(user)
+        return userManager
+    }
+}
