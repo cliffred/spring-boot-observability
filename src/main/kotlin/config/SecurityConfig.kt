fc784bf7aab40bf5e521c0e2020e3b6cf042a936
Cliffred van Velzen <cliffred@cliffred.nl>
2024-01-26 18:10:22 +0100

    Partly secure actuator endpoints

--- src/main/kotlin/config/SecurityConfig.kt
+++ src/main/kotlin/config/SecurityConfig.kt
@@ -1,9 +1,16 @@
 package red.cliff.observability.config
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
+import org.springframework.boot.actuate.health.HealthEndpoint
+import org.springframework.boot.actuate.info.InfoEndpoint
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.HttpSecurityDsl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
@@ -14,35 +21,73 @@ import org.springframework.security.provisioning.UserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import javax.sql.DataSource
 
-@EnableWebSecurity
 @Configuration
-class SecurityConfig {
-    @Bean
-    fun securityFilterChain(
-        http: HttpSecurity,
-        securityExceptionHandler: SecurityExceptionHandler,
-    ): SecurityFilterChain {
-        http {
-            authorizeRequests {
-                authorize("/api/**", authenticated)
-                authorize("/actuator/**", hasRole("ADMIN"))
-                authorize(anyRequest, permitAll)
-            }
-            httpBasic { }
-            exceptionHandling {
-                accessDeniedHandler = securityExceptionHandler
-                authenticationEntryPoint = securityExceptionHandler
-            }
-        }
-        return http.build()
-    }
-
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(private val securityExceptionHandler: SecurityExceptionHandler) {
     @Bean
     fun dataSource(): DataSource {
         return EmbeddedDatabaseBuilder()
             .setType(EmbeddedDatabaseType.H2)
             .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
             .build()
+    }
+
+    @Bean
+    @Order(1)
+    fun openActuatorEndpoint(http: HttpSecurity): SecurityFilterChain {
+        http {
+            securityMatcher(
+                EndpointRequest.to(
+                    HealthEndpoint::class.java,
+                    InfoEndpoint::class.java,
+                    PrometheusScrapeEndpoint::class.java,
+                ),
+            )
+            authorizeRequests {
+                authorize(anyRequest, permitAll)
+            }
+            sharedConfig()
+        }
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    fun securedActuatorEndpoint(http: HttpSecurity): SecurityFilterChain {
+        http {
+            securityMatcher(
+                EndpointRequest.toAnyEndpoint(),
+            )
+            authorizeRequests {
+                authorize(anyRequest, hasRole("ADMIN"))
+            }
+            sharedConfig()
+        }
+        return http.build()
+    }
+
+    @Bean
+    @Order(3)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeRequests {
+                authorize("/api/**", authenticated)
+                authorize(anyRequest, permitAll)
+            }
+            sharedConfig()
+        }
+        return http.build()
+    }
+
+    private fun HttpSecurityDsl.sharedConfig() {
+        httpBasic {
+            authenticationEntryPoint = securityExceptionHandler
+        }
+        exceptionHandling {
+            accessDeniedHandler = securityExceptionHandler
+            authenticationEntryPoint = securityExceptionHandler
+        }
     }
 
     @Bean
@@ -53,8 +98,15 @@ class SecurityConfig {
                 .password("user")
                 .roles("USER")
                 .build()
-        val userManager = JdbcUserDetailsManager(dataSource)
-        userManager.createUser(user)
-        return userManager
+        val admin =
+            User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN", "USER")
+                .build()
+        return JdbcUserDetailsManager(dataSource).apply {
+            createUser(user)
+            createUser(admin)
+        }
     }
 }
