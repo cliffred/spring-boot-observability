83bd61106d4852ed578a670ada76312c8f93caa5
Cliffred van Velzen <cliffred@cliffred.nl>
2025-02-03 11:52:34 +0100

    Add api-call test back again

--- src/test/kotlin/trace/TraceControllerTest.kt
+++ src/test/kotlin/trace/TraceControllerTest.kt
@@ -1,14 +1,20 @@
 package red.cliff.observability.trace
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.marcinziolo.kotlin.wiremock.contains
+import com.marcinziolo.kotlin.wiremock.equalTo
 import com.marcinziolo.kotlin.wiremock.get
+import com.marcinziolo.kotlin.wiremock.returnsJson
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.ints.shouldBeInRange
+import io.kotest.matchers.maps.shouldHaveKeys
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldHaveLength
+import io.kotest.matchers.types.shouldBeTypeOf
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 import red.cliff.observability.IntegrationTest
+import java.util.UUID
 
 @IntegrationTest
 @AutoConfigureWireMock(port = 0)
@@ -26,6 +32,40 @@ class TraceControllerTest(
                     .isOk
                     .expectBody<String>()
                     .isEqualTo("OK")
+            }
+
+            should("call httpBin API and propagate trace id") {
+                val traceId = UUID.randomUUID().toString().replace("-", "")
+                val parentId = traceId.take(16).reversed()
+                val traceParent = "00-$traceId-$parentId-01"
+
+                wiremock.get {
+                    url equalTo "/get"
+                } returnsJson {
+                    body = """
+                         {
+                          "headers": {
+                            "Traceparent": "$traceParent"
+                          }
+                        }
+                          """
+                }
+
+                webTestClient
+                    .get()
+                    .uri("/trace/api-call")
+                    .header("traceparent", traceParent)
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBody<Map<String, *>>()
+                    .value {
+                        it.shouldHaveKeys("traceparentHeader")
+                        val traceParentHeader = it["traceparentHeader"]
+                        traceParentHeader.shouldBeTypeOf<String>()
+                        traceParentHeader shouldHaveLength 55
+                        traceParentHeader shouldContain traceId
+                    }
             }
 
             should("return random number") {
